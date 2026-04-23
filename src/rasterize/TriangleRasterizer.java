2c25a35159903b82ee441b0e7356269c71d89865
Václav Sobotka <sobotka.vaclav@gmail.com>
2026-04-23 11:54:42 +0200

    Complete rasterizer impl

--- src/rasterize/TriangleRasterizer.java
+++ src/rasterize/TriangleRasterizer.java
@@ -13,31 +13,53 @@ public class TriangleRasterizer {
     }
 
     public void rasterize(Vertex a, Vertex b, Vertex c) {
-        // TODO: seřadit vrcholy podle y od min po max
+        Vertex v1 = a, v2 = b, v3 = c;
+        if (v1.getY() > v2.getY()) { Vertex t = v1; v1 = v2; v2 = t; }
+        if (v2.getY() > v3.getY()) { Vertex t = v2; v2 = v3; v3 = t; }
+        if (v1.getY() > v2.getY()) { Vertex t = v1; v1 = v2; v2 = t; }
 
         Lerp<Vertex> lerp = new Lerp<>();
 
         // 1. část
-        for (int y = (int) a.getY(); y <= (int) b.getY(); y++) {
-            // Hrana AB
-            double tAB = (y - a.getY()) / (b.getY() - a.getY());
-            Vertex ab = lerp.lerp(a, b, tAB);
+        for (int y = (int) v1.getY(); y <= (int) v2.getY(); y++) {
+            // Hrana V12
+            double tAB = (y - v1.getY()) / (v2.getY() - v1.getY());
+            Vertex v12 = lerp.lerp(v1, v2, tAB);
 
-            // Hrana AC
-            double tAC = (y - a.getY()) / (c.getY() - a.getY());
-            Vertex ac = lerp.lerp(a, c, tAC);
+            // Hrana V13
+            double tAC = (y - v1.getY()) / (v3.getY() - v1.getY());
+            Vertex v13 = lerp.lerp(v1, v3, tAC);
 
-            // TODO: kontrola, jestli je ab.getX() < ac.getX()
+            Vertex x1 = v12, x2 = v13;
+            if (x1.getX() > x2.getX()) { Vertex t = x1; x1 = x2; x2 = t; }
 
-            for (int x = (int) Math.round(ab.getX()); x <= (int) Math.round(ac.getX()); x++) {
-                double t = (x - ab.getX()) / (ac.getX() - ab.getX());
-                Vertex pixel = lerp.lerp(ab, ac, t);
+            for (int x = (int) Math.round(x1.getX()); x <= (int) Math.round(x2.getX()); x++) {
+                double t = (x - x1.getX()) / (x2.getX() - x1.getX());
+                Vertex pixel = lerp.lerp(x1, x2, t);
 
                 zBuffer.setPixelWithZTest(x, y, pixel.getZ(), pixel.getColor());
             }
         }
 
         // 2. část
+        for (int y = (int) v2.getY(); y <= (int) v3.getY(); y++) {
+            // Hrana V13 (dlouhá, aktivní v obou polovinách)
+            double tAC = (y - v1.getY()) / (v3.getY() - v1.getY());
+            Vertex v13 = lerp.lerp(v1, v3, tAC);
 
+            // Hrana V23 (krátká, spodní polovina)
+            double tBC = (y - v2.getY()) / (v3.getY() - v2.getY());
+            Vertex v23 = lerp.lerp(v2, v3, tBC);
+
+            Vertex x1 = v13, x2 = v23;
+            if (x1.getX() > x2.getX()) { Vertex t = x1; x1 = x2; x2 = t; }
+
+            for (int x = (int) Math.round(x1.getX()); x <= (int) Math.round(x2.getX()); x++) {
+                double t = (x - x1.getX()) / (x2.getX() - x1.getX());
+                Vertex pixel = lerp.lerp(x1, x2, t);
+
+                zBuffer.setPixelWithZTest(x, y, pixel.getZ(), pixel.getColor());
+            }
+        }
     }
 }
