87b9aa2b934bab17a5ae964cfde77ba7b38a49c5
Václav Sobotka <sobotka.vaclav@gmail.com>
2026-05-10 15:55:08 +0200

    Move solids around for better composition

--- src/controller/Controller3D.java
+++ src/controller/Controller3D.java
@@ -82,8 +82,9 @@ public class Controller3D {
         this.arrowZ.setModel(new Mat4RotY(Math.toRadians(-90)));
 
         this.sphere = new Sphere(new Vec3D(0, 0, 0), 1);
-        this.cube = new Cube(new Vec3D(2, 2, 0), 0.5);
-        this.cone = new Cone(new Vec3D(-2, 2, 0), 0.5, 1.2);
+        this.cube = new Cube(new Vec3D(0.5, 0, 0.5), 0.5);
+        this.cone = new Cone(new Vec3D(-0.5, 0.3, -1), 0.5, 3);
+        this.cone.setModel(new Mat4RotX(Math.toRadians(45)));
 
         selectableSolids.add(sphere);
         selectableSolids.add(cube);
